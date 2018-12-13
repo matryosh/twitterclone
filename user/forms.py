1b22c157a227595de7c49bf164d19d3d6f0d388c
trevor <tbrick1234@gmail.com>
2018-12-12 23:30:39 -0500

    created a test for the custom user model.

--- user/forms.py
+++ user/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+User = get_user_model()
+
+
+class CreateUserTestCase(TestCase):
+
+    def setUp(self):
+
+        self.user = User(
+            age=25,
+            email="oceanman@gmail.com",
+            username="spongebob",
+            password="patrick"
+        )
+
+    def test_user(self):
+        self.assertEqual(self.user.email, "oceanman@gmail.com", "These emails aren't equal")
+        self.assertEqual(self.user.age, 25, "These ages aren't equal")
+        self.assertEqual(self.user.username, "spongebob", "These usernames aren't equal")
+        self.assertEqual(self.user.password, "patrick", "These passwords aren't equal")
