ef5484facb974ec57ff719aeb8fca7a86c818a39
trevor <tbrick1234@gmail.com>
2019-02-12 14:33:46 -0500

    fixed a bug where the extension didn't save properly.

--- user/renameandupload.py
+++ user/renameandupload.py
@@ -6,6 +6,6 @@ def path_and_rename(instance, filename):
     upload_to = 'media/images'
     ext = filename.split('.')[-1]
 
-    filename = '{}{}'.format(uuid4().hex, ext)
+    filename = '{}.{}'.format(uuid4().hex, ext)
 
     return os.path.join(upload_to, filename)
